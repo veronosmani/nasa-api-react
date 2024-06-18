10d6190cbd7de73ba8b93218e686e4a77673037d
veronosmani <veron.osmani@gmail.com>
2024-06-18 09:35:32 +0200

    new file:   src/assets/example.jpg 	modified:   src/components/Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import example from '../assets/example.jpg'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div>
+        <img src={example}
+    </div>
   )
 }
