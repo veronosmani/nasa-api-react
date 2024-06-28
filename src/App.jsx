8855fc7fff7b519139d58a84270aa51fa1fad919
veronosmani <veron.osmani@gmail.com>
2024-06-28 16:52:38 +0200

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,51 @@
-import { useEffect, useState } from "react"
-import Footer from "./components/Footer"
-import Main from "./components/Main"
-import SideBar from "./components/SideBar"
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SideBar from "./components/SideBar";
 
 function App() {
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [showModal, setShowModal] = useState(false)
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   function handleToggleModal() {
-    setShowModal(!showModal)
+    setShowModal(!showModal);
   }
 
   useEffect(() => {
     async function fetchAPIData() {
-      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
-      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
 
-
-      const today = (new Date()).toDateString()
-      const localKey = `NASA-${today}`
+      const today = new Date().toDateString();
+      const localKey = `NASA-${today}`;
       if (localStorage.getItem(localKey)) {
-        const apiData = JSON.parse(localStorage.getItem(localKey))
-        setData(apiData)
-        console.log('Fetched from cache today')
-        return
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        console.log("Fetched from cache today");
+        return;
       }
-      localStorage.clear()
+      localStorage.clear();
 
       try {
-        const res = await fetch(url)
-        const apiData = await res.json()
-        localStorage.setItem(localKey, JSON.stringify(apiData))
-        setData(apiData)
-        console.log('Fetched from API today')
+        const res = await fetch(url);
+        const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData));
+        setData(apiData);
+        console.log("Fetched from API today");
       } catch (err) {
-        console.log(err.message)
+        console.log(err.message);
       }
     }
-    fetchAPIData()
-  }, [])
+    fetchAPIData();
+  }, []);
 
   return (
     <>
-      {data ? (<Main data={data} />) : (
+      {data ? (
+        <Main data={data} />
+      ) : (
         <div className="loadingState">
           <i className="fa-solid fa-gear"></i>
         </div>
@@ -51,11 +53,9 @@ function App() {
       {showModal && (
         <SideBar data={data} handleToggleModal={handleToggleModal} />
       )}
-      {data && (
-        <Footer data={data} handleToggleModal={handleToggleModal} />
-      )}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
